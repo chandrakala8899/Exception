a26f84a0b01d870f5659fa60893c289096761d09
chandrakala <chandraklag688@gmail.com>
2024-03-05 10:55:27 +0530

    exception

---
+++ src/Exception/CheckedException.java
@@ -0,0 +1,19 @@
+package Exception;
+
+public class CheckedException {
+ String userName;
+ String passWord;
+
+    public CheckedException(String userName, String passWord) {
+        this.userName = userName;
+        this.passWord = passWord;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+}
